7b6c8303b7c7c1a0b2d36db9956f972edab02e04
1Andry <rivsmile@yandex.ru>
2021-08-18 18:32:03 +0300

    fizzBuzz

---
+++ number.js
@@ -0,0 +1,20 @@
+const FizzBuzz = (begin,end)=>{
+    for (let i = begin; begin < end; i++) { 
+        if (i%3===0 && i%5===0){
+            console.log("FizzBuzz");
+        }
+        else if(i%5===0){
+            console.log("Buzz");
+        }
+        else if(i%3===0){
+            console.log("Fizz");
+        }
+        else if(i>end){
+            break;
+        }
+        else{
+            console.log(i);
+        }
+    }
+    }
+    console.log(FizzBuzz(0,44))
