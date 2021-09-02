c0dd72fc7e77bfeb4b6c89f3b679f4a1ef8e978d
1Andry <rivsmile@yandex.ru>
2021-09-02 11:46:34 +0300

    сложить входящие цифры пока не останется одна

---
+++ sumOfIndex.js
@@ -0,0 +1,15 @@
+const sumIndwxNum = num => {
+    num = String(num);
+    let i = 0
+    let result = 0;
+    while (i < num.length){
+    result += Number(num[i])
+    i++;
+    }
+    if (result>9){
+        return sumIndwxNum(result)
+    }
+    else return result;
+};
+
+console.log(sumIndwxNum (38));
