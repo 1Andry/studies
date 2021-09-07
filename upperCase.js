6a8b00961036da4948ea75ead58fe0e6629693ae
1Andry <rivsmile@yandex.ru>
2021-09-07 20:26:36 +0300

    верхний регистр первой буквы в массиве

---
+++ upperCase.js
@@ -0,0 +1,8 @@
+
+const text = ["hello", "big", "world"];
+for(let i = 0; i < text.length; i++){
+    const newText = text[i][0].toUpperCase() + text[i].slice(1)
+    //            = обращаемся к массиву к первой букве первого  значения и переводим ее в верхний регистр
+    //            + добавляем оставшиеся буквы кроме первой "начиная с 1-го индекса"   
+    console.log(newText)
+};
