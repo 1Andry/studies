dd41d98b362d44e0c80606e834306020ad7d1fc8
1Andry <rivsmile@yandex.ru>
2021-09-08 20:19:26 +0300

    натуральная степень 3

---
+++ divisionСheck3.js
@@ -0,0 +1,13 @@
+const divisionChek = num => {
+    for (i = 0; i <= num; i++){
+        if( num === Math.pow(3,i) ){
+            console.log(i)
+        return true     
+        }      
+        else {
+            false
+        } 
+        
+    }  
+};
+console.log(divisionChek(8))
