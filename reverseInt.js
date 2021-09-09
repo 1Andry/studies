bf08c407b1540ca878aa1279f6b15d01cd3e8986
1Andry <rivsmile@yandex.ru>
2021-09-09 20:24:27 +0300

    набросок

---
+++ reverseInt.js
@@ -0,0 +1,15 @@
+const reversInt = num =>{
+    let result = 0
+    let a = num[0]
+    let b = "-"
+    if (a === b)
+        for(let i =0; i<=num; i++){
+            
+        }
+
+    
+    console.log(b)
+    
+
+}
+console.log(reversInt(-12))
