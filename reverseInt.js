852067999c0b205c61d0db53c6dcc8f08668923d
1Andry <rivsmile@yandex.ru>
2021-09-12 10:05:13 +0300

    разворачивает число

--- reverseInt.js
+++ reverseInt.js
@@ -1,15 +1,19 @@
-const reversInt = num =>{
-    let result = 0
-    let a = num[0]
+const reversInt = num => { 
+    num = String(num)
+    let result = " "
+    let a = String(num[0])
     let b = "-"
-    if (a === b)
-        for(let i =0; i<=num; i++){
-            
+    if (a === b){
+        for(let i = num.length -1; i > 0; i--){
+            result += num[i]
+        }    
+        return `${a}  ${result}` 
+        
+    }  else {
+        for(let i = num.length -1; i >= 0; i--){
+            result += num [i]
         }
-
-    
-    console.log(b)
-    
-
-}
-console.log(reversInt(-12))
+        return result
+    }      
+};
+console.log(reversInt(-123));
