f090aaa93573c912723c8efd923088567a43ddcc
1Andry <rivsmile@yandex.ru>
2021-09-16 18:49:26 +0300

    кошелек с деньгами

---
+++ moneyBag.js
@@ -0,0 +1,11 @@
+const money = [ "usd 2", "eur 4", "usd 8", "eur 4", "rub 50",]
+const getTotalMoney = (money, name) => {
+    let result = 0;
+    for(let i = 0; i < money.length; i++) {
+        let currency = money[i].split(" ")
+        if(currency[0] === name)
+        result += Number (currency[1]);
+    }
+    return result;
+};
+console.log(getTotalMoney (money, "eur"));
